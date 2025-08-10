945758e47755cd6f71f46963324fc528045fa730
Jasweer09 <jasweertadikonda@gmail.com>
2025-08-10 13:43:21 -0400

    changes required for app.py

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ import traceback
 from xgb_ensemble import XGBEnsemble
 from flask_cors import CORS
 
+class XGBEnsemble:
+    def __init__(self, model_borderline, model_native, threshold=0.3):
+        self.model_borderline = model_borderline
+        self.model_native = model_native
+        self.threshold = threshold
+
+    def predict_proba(self, X):
+        proba_borderline = self.model_borderline.predict_proba(X)[:, 1]
+        proba_native = self.model_native.predict_proba(X)[:, 1]
+        proba_avg = (proba_borderline + proba_native) / 2
+        return np.vstack([1 - proba_avg, proba_avg]).T
+
+    def predict(self, X):
+        #print(X)
+        print(self.predict_proba(X))
+        return (self.predict_proba(X)[:, 1] >= 0.5).astype(int)
+
 app = Flask(__name__)
 CORS(app)
 
